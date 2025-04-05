fac8095546f8133b0214a14c06dc7f45dc52c198
Wender13 <77487372+Wender13@users.noreply.github.com>
2025-04-05 11:08:29 -0300

    CORS configurado para receber o front-end.

---
+++ src/main/java/dev/jr/SpringBank/security/CorsConfig.java
@@ -0,0 +1,28 @@
+package dev.jr.SpringBank.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // ou "*", se preferir liberar tudo
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true); // se for usar cookies/autenticação
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
+    }
+}
